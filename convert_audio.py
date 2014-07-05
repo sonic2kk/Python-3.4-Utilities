0014818177fd4303fd12c6e8044a4e0a66213496
sonic2kk <Eamonn@Eamonns-MacBook-Pro.local>
2014-07-05 12:19:06 +0100

    Minor changes

--- convert_audio.py
+++ convert_audio.py
@@ -1,33 +1,55 @@
 #/usr/bin/env python
 
+# Importing the OS module.
 import os
 
+# Importing the PyDub library to convert the audio files.
 from pydub import AudioSegment
 
 
+# Converter class.
 class Convert():
+	# Init method.
 	def __init__(self):
+		# Extension, extension to change and directory variable.
 		self.extension = input("What extension should I convert to? ")
 		self.extension_to_look_for = input("What extension should I look for? ")
 		self.directory = input("What directory are the file(s) located? ")
 
+		# Calling the main method. This does all the heavy lifting.
 		self.main()
 
 	def main(self):
+		# Replacing "~" with the home directory of the user.
 		if self.directory.startswith("~"):
 			self.directory = self.directory.replace("~", os.path.expanduser("~"))
 
+		# Changing into the directory the user specified.
 		os.chdir(self.directory)
 
+		# Looping through all files in our current directory.
 		for f in os.listdir():
+			# If our file does not begin with "."
+			
+			# This is because on some operating systems, files BEGINNING with
+			# "." are hidden files, are are usually meant to be ignored as they
+			# are mainly (or only) used by the system. On OS X, an example would
+			# be the ".DS_STORE" file.
 			if not f.startswith("."):
+				# Printing the file we are converting.
 				print("Converting: " + f + "...")
 
+				# Loading the file using PyDub.
 				file_to_convert = AudioSegment.from_file(f, self.extension_to_look_for)
 
+				# Converting the file using PyDub.
 				file_to_convert.export(f, format=self.extension)
 
+				# Printing a success message. This will only be printed if the
+				# conversion is successful.
 				print("Converted " + f + " Successfully!")
 
 
+# Creating a "Convert" object from the "Convert" class, and
+# calling it "c".
 c = Convert()
--- replace_extension.py
+++ replace_extension.py
@@ -1,26 +1,48 @@
 #!/usr/bin/env python
 
+# Importing the OS module.
 import os
 
 
+# ReplaceExtension class.
 class ReplaceExtension():
+    # Init method.
     def __init__(self):
+        # Extention to change, extension to look for and directory.
+        # variables
         self.extension_to_change = input("What extension should I change? ")
         self.extension_to_look_for = input("What extension should I look for? ")
         self.directory = input("What directory are the file(s) located? ")
 
+        # Replacing the "~" symbol with the users home directory.
         if self.directory.startswith("~"):
         	self.directory = self.directory.replace("~", os.path.expanduser("~"))
 
+        # Calling the main method, which does all the heavy lifting
         self.main()
 
     def main(self):
+        # Changing into the directory that the user has specified
         os.chdir(self.directory)
 
+        # Looping through all directories in our current directory
         for f in os.listdir():
+            # If the file ends with the extension we are looking for
         	if f.endswith(self.extension_to_look_for):
+                # Storing a replaced version of our file in a string.
+                # The reason for this is because in Python, Strings are
+                # immutatible, meaning they cannot be changed. To work
+                # around this, we store what the string would read in
+                # a variable. This means that this does not replace the
+                # the extension of the file in this line.
         		fi = f.replace(self.extension_to_look_for, self.extension_to_change)
+
+                # Instead, this is done here. This uses the OS module to rename
+                # the file, since strings are not mutatible from within Python,
+                # we edit it on a system level (or on an OS (Operating System)
+                # level).
         		os.rename(f, fi)
 
 
-c = ReplaceExtension()
+# Creating an instance of the "ReplaceExtension" class, calling it "re".
+re = ReplaceExtension()
